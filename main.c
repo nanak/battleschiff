21efa8d2d2c019826e67f8ab55c41a00c1d1cff2
XDDominik <Dominik.Backhausen@gmail.com>
2012-12-09 13:31:33 +0100

    main.cpp in main.c umbenannt
    eingabe ausgabe tests durchgeführt
    sowie die funktion von Pointern getestet

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+
+
+int main(){
+	char *test[]={"red","green"};
+
+	char ca [20];
+	printf("Baztleschiff von Dominik Backhausen, Nanak Tattyrek und Thomas Traxler \nBitte neu fuer ein neues Spiel eingeben! \n");
+	scanf("%s",ca);
+	printf("%s",ca);
+	printf("\n");
+	printf("%s",*(test+1));
+	printf("\n");
+	scanf("%s",*(test+2));
+	printf("%s",*(test+2));
+}  
+
